dceb1130cd25155209daed4edc9bd415bacfd5b6
Hossain <arafat.kd99@gmail.com>
2023-11-30 22:09:40 +0100

    Add transactions to your queries.sql file

--- queries.sql
+++ queries.sql
@@ -16,3 +16,61 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN 10.4 AND 17.3;
 
+--setting the species column to unspecified
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+
+--roll back the change and verify that the species columns went back to null
+ROLLBACK;
+SELECT * FROM animals;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species = 'pokemon'
+WHERE species IS NULL;
+
+SELECT * FROM animals;
+COMMIT;
+
+--Verify that changes persist after commit.
+SELECT * FROM animals;
+
+--delete all records in the animals table
+BEGIN;
+DELETE FROM animals;
+
+--roll back the transaction
+ROLLBACK;
+--verify all records in the animals table still exists.
+SELECT * FROM animals;
+
+-- Start a transaction
+BEGIN;
+-- Delete all animals born after Jan 1st, 2022
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+
+-- Create a savepoint for the transaction
+SAVEPOINT weight_update;
+
+-- Update all animals' weight to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = weight_kg * -1;
+
+-- Rollback to the savepoint
+ROLLBACK TO weight_update;
+
+-- Update all animals' weights that are negative to be their weight multiplied by -1
+UPDATE animals
+SET weight_kg = ABS(weight_kg)
+WHERE weight_kg < 0;
+
+COMMIT;
+
+
