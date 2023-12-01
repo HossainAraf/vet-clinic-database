7d248a31e52654eaf1ef3906ad4b739dec2dacde
Hossain <arafat.kd99@gmail.com>
2023-12-01 23:03:47 +0100

    Replace column species from species_id

--- data.sql
+++ data.sql
@@ -15,3 +15,14 @@ VALUES
     (9, 'Boarmon', '2005-06-07', 7, true, 20.4),
     (10, 'Blossom', '1998-10-13', 3, true, 17),
     (11, 'Ditto', '2022-05-14', 4, true, 22);
+
+    --INSERT full-name and age into owners table
+INSERT INTO owners (full_name, age)
+VALUES
+    ('Sam Smith', 34),
+    ('ennifer Orwell', 19),
+    ('Bob ', 45),
+    ('Melody Pond', 77),
+    ('Dean Winchester', 14),
+    ('Jodie Whittaker', 38);
+    
