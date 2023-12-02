216f7ef7ec3805a16182ce2a44d739c75dbb598a
Hossain <arafat.kd99@gmail.com>
2023-12-02 20:55:17 +0100

    Insert data for vets

--- data.sql
+++ data.sql
@@ -42,3 +42,10 @@ SET owner_id =
         WHEN name IN ('Angemon', 'Boarmon') THEN 5
         ELSE NULL
     END;
+
+    -- INSERT DATA FOR VETS
+INSERT INTO vets (name, age, date_of_graduation) VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
