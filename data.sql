a2c1d2689ec56694bdf935b95f008dd4798e3f70
Hossain <arafat.kd99@gmail.com>
2023-12-02 20:59:01 +0100

    Insert data for visits

--- data.sql
+++ data.sql
@@ -57,3 +57,26 @@ INSERT INTO specialization (vet_id, species_id) VALUES
 (3, 1), -- Stephanie Mendez specialized in Pokemon
 (4, 2); -- Jack Harkness specialized in Digimon
 
+-- INSERT DATA FOR VISITS
+INSERT INTO visits (animal_id, vet_id, date_of_visit) VALUES
+(1, 1, '2020-05-24'), -- Agumon visited William Tatcher
+(1, 3, '2020-07-22'), -- Agumon visited Stephanie Mendez
+(2, 4, '2021-02-02'), -- Gabumon visited Jack Harkness
+(3, 2, '2020-01-05'), -- Pikachu visited Maisy Smith
+(3, 2, '2020-03-08'),
+(3, 2, '2020-05-14'),
+(4, 3, '2021-05-04'), -- Devimon visited Stephanie Mendez
+(5, 4, '2021-02-24'), -- Charmander visited Jack Harkness
+(6, 2, '2019-12-21'), -- Plantmon visited Maisy Smith
+(6, 1, '2020-08-10'), -- Plantmon visited William Tatcher
+(6, 2, '2021-04-07'),
+(7, 3, '2019-09-29'), -- Squirtle visited Stephanie Mendez
+(8, 4, '2020-10-03'), -- Angemon visited Jack Harkness
+(8, 4, '2020-11-04'),
+(9, 2, '2019-01-24'), -- Boarmon visited Maisy Smith
+(9, 2, '2019-05-15'),
+(9, 2, '2020-02-27'),
+(9, 2, '2020-08-03'),
+(10, 3, '2020-05-24'), -- Blossom visited Stephanie Mendez
+(10, 1, '2021-01-11'); -- Blossom visited William Tatcher
+
